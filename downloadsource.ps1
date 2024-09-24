e8e124a3b477f6de9a4cc9bf4310164751eb0285
aeycue <aeycue@yahoo.com>
2024-09-24 14:56:35 -0500

    Create downloadsource.ps1

---
+++ downloadsource.ps1
@@ -0,0 +1,3 @@
+Start-Process -FilePath "c:\temp\azcopy.exe"  -ArgumentList "-copy 'https://softwarebuilder.blob.core.windows.net/softwarerepo/Source.zip?sp=r&st=2024-09-24T19:27:53Z&se=2024-10-02T03:27:53Z&spr=https&sv=2022-11-02&sr=b&sig=ccdUS5B9dS0RnmFnvaWM8iRz3%2F6fRRkDQc50PCbKvK0%3D' c:\temp
+"
+expand-archive 'c:\temp\source.zip' c:\
