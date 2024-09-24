d5f564e415d44b9ec45beedc8322624503283846
aeycue <aeycue@yahoo.com>
2024-09-24 15:07:44 -0500

    Update downloadsource.ps1

--- downloadsource.ps1
+++ downloadsource.ps1
@@ -2,6 +2,7 @@ $azcopy = "C:\temp\azcopy.exe"
 $src = "https://softwarebuilder.blob.core.windows.net/softwarerepo/Source.zip?sp=r&st=2024-09-24T19:27:53Z&se=2024-10-02T03:27:53Z&spr=https&sv=2022-11-02&sr=b&sig=ccdUS5B9dS0RnmFnvaWM8iRz3%2F6fRRkDQc50PCbKvK0%3D"
 $dest = "c:\temp"
 $fullline = $azcopy + " -copy" + " " + $src + " " + $dest
-$fullline
-& $fullline
-expand-archive 'c:\temp\source.zip' c:\
+write-host $fullline
+#& $fullline
+
+#expand-archive 'c:\temp\source.zip' c:\
