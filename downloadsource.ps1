6ba8c6c6633f0856dc70744d463e1da33a29b2a2
aeycue <aeycue@yahoo.com>
2024-09-25 14:07:26 -0500

    Update downloadsource.ps1

--- downloadsource.ps1
+++ downloadsource.ps1
@@ -1,6 +1,6 @@
 cd c:\temp
 $azcopy = "C:\temp\azcopy.exe"
-$src = "https://softwarebuilder.blob.core.windows.net/softwarerepo/Source.zip?sp=r&st=2024-09-24T19:27:53Z&se=2024-10-02T03:27:53Z&spr=https&sv=2022-11-02&sr=b&sig=ccdUS5B9dS0RnmFnvaWM8iRz3%2F6fRRkDQc50PCbKvK0%3D"
+$src = "https://softwarebuilder.blob.core.windows.net/softwarerepo/Source.zip?sp=r&st=2024-09-25T19:05:45Z&se=2024-12-07T04:05:45Z&spr=https&sv=2022-11-02&sr=b&sig=I21nTcYVk11jmHbjCbwxzbLncbFwz8YmO5DIRt2u3o8%3D"
 $dest = "c:\temp"
 .\azcopy copy $src $dest
 expand-archive 'c:\temp\source.zip' c:\
