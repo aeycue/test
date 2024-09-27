b73a27de17f65ba97d20424e19347eb2c8ead284
aeycue <aeycue@yahoo.com>
2024-09-27 13:29:20 -0500

    Update installapps.ps1

--- installapps.ps1
+++ installapps.ps1
@@ -10,8 +10,9 @@ function Write-Log {
 }
 #endregion
 
-$appname = "CrowdStrike"
 #region crowdstrike
+$appname = "CrowdStrike"
+Write-Log "$($appname)
 try {
     & "C:\Source\CrowdStrike_FalconPrevent_6.38.15205.0\Deploy\install.ps1"
     if (Test-Path "C:\Program Files\Crowdstrike\CSFalconContainer.exe") {
@@ -27,8 +28,10 @@ catch {
 }
 #endregion
 
-$appname = "ForcePoint"
+
 #region ForcePoint
+$appname = "ForcePoint"
+Write-Log "$($appname)
 try {
     & "C:\Source\Forcepoint_DLP_22.12.5622\Deploy\Install_ForcePoint_DLP.ps1"
     if (Test-Path "c:\Program Files\Websense\Websense Endpoint\EndPointClassifier.exe") {
